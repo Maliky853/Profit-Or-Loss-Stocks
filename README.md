3d231dd6716b52e99ffbb901483d9362cb2d66b9
Maliky853 <maliky853@gmail.com>
2022-08-13 13:10:39 +0530

    updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Profit-Or-Loss-Stocks
+ Profit and Loss Stocks
+In this app we can calculate the profit and loss of the stock by putting the initial price, quantity and current price of the stock. The output will show the profit and loss in Percentage and Absolute value.
+
+
+How to Use
+Enter the initial stock price
+
+Enter the Quantity of Stock
+
+Enter the current stock price
+
+Click the button
+
+Output will shown below the button
+
+Programming Language
+Html
+
+Css
+
+Javascript
--- index.js
+++ index.js
@@ -6,31 +6,37 @@ const outputDiv = document.querySelector("#output-div");
 
 function calculateProfitAndLoss(initial, quantity, current) {
   if (initial > current) {
-    let loss = (initial - current) * quantity;
-    let lossPercentage = (loss / initial) * 100;
+    let loss = ((initial - current) * quantity);
+    let lossPercentage = ((loss/(initial*quantity))*100);
 
     outputDiv.innerText = `Your Loss Is ${loss} 
 And The Loss Percent Is ${lossPercentage}%`;
   } else if (current > initial) {
-    let profit = (current - initial) * quantity;
-    let profitPercentage = (profit / initial) * 100;
+    let profit = ((current - initial) * quantity);
+    let profitPercentage = ((profit/(initial*quantity))*100);
 
     outputDiv.innerText = `Your Profit Is ${profit} And The Profit Percent Is ${profitPercentage}% `;
   } else if (initial === current) {
-    outputDiv.innerText = `Ruko Zara Sabr Karo!`;
+    outputDiv.innerText = `No Profit No Loss`;
   }
 }
 
-// calculateProfitAndLoss(100,10,10)
-// calculateProfitAndLoss(20,20,100)
-// calculateProfitAndLoss(10.10,10)
+
 
 function submitHandler() {
   let ip = Number(initialPrice.value);
   let qty = Number(stockQuantity.value);
   let curr = Number(currentPrice.value);
 
+    if(!ip || !qty || !curr){
+      outputDiv.innerText = "Please input all the feild"
+    }else if(ip<0 || qty<0 || curr<0){
+      outputDiv.innerText = "The Value Should Not Be Negative"
+    }
+    else{
   calculateProfitAndLoss(ip, qty, curr);
+    }
 }
 
 submitBtn.addEventListener("click", submitHandler);
+
